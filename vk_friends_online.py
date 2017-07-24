15892a7b75290bc5a1e9e14fe32fe63c9418cae7
KhorinVitaly <khorin.v@gmail.com>
2017-07-24 18:23:43 +0600

    first commit, my solution by task

--- vk_friends_online.py
+++ vk_friends_online.py
@@ -1,15 +1,16 @@
 import vk
+import getpass
 
 
-APP_ID = -1  # чтобы получить app_id, нужно зарегистрировать своё приложение на https://vk.com/dev
+APP_ID = 6121431
 
 
 def get_user_login():
-    pass
+    return input("Input your login:")
 
 
 def get_user_password():
-    pass
+    return getpass.unix_getpass("Input your password: ")
 
 
 def get_online_friends(login, password):
@@ -17,16 +18,28 @@ def get_online_friends(login, password):
         app_id=APP_ID,
         user_login=login,
         user_password=password,
+        scope="friends"
     )
     api = vk.API(session)
-    # например, api.friends.get()
+    friends_online_ids = api.friends.getOnline()
+    return api.friends.get(list_id=friends_online_ids, fields="first_name, last_name")
 
 
 def output_friends_to_console(friends_online):
-    pass
+    if friends_online:
+        print("{0} friends online:".format(len(friends_online)))
+        for friend in friends_online:
+            print("{0} {1}".format(friend["first_name"], friend["last_name"]))
+    else:
+        print("There aren't friends online.")
+
+
 
 if __name__ == '__main__':
     login = get_user_login()
     password = get_user_password()
-    friends_online = get_online_friends(login, password)
-    output_friends_to_console(friends_online)
+    try:
+        friends_online = get_online_friends(login, password)
+        output_friends_to_console(None)
+    except vk.exceptions.VkAuthError:
+        print("Should try again.")
